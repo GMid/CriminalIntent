9d7861ba93426bae3601ec691a9a9e4fbff243c9
George Middleton <geo.middleton@gmail.com>
2018-10-15 16:24:20 -0700

    End of Chapter 7

---
+++ app/src/main/java/edu/pdx/ece558f18/gmid2/criminalintent/CrimeFragment.java
@@ -0,0 +1,87 @@
+package edu.pdx.ece558f18.gmid2.criminalintent;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+
+import edu.pdx.ece558f18.gmid2.criminalintent.Crime;
+import edu.pdx.ece558f18.gmid2.criminalintent.R;
+
+public class CrimeFragment extends Fragment
+{
+    private Crime mCrime;
+    private EditText mTitleField;
+    private Button mDateButton;
+    private CheckBox mSolvedCheckbox;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        mCrime = new Crime();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState)
+    {
+        super.onCreateView(inflater, container, savedInstanceState);
+        View v = inflater.inflate(R.layout.crime_fragment, container, false);
+
+        mTitleField = v.findViewById(R.id.crime_title);
+        mTitleField.addTextChangedListener(new TextWatcher()
+        {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after)
+            {
+                // Intentially blank
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count)
+            {
+                mCrime.setTitle(s.toString());
+            }
+
+            @Override
+            public void afterTextChanged(Editable s)
+            {
+                // Intentially blank
+            }
+        });
+
+        mDateButton = v.findViewById(R.id.crime_date);
+        mDateButton.setText(mCrime.getDate().toString());
+        mDateButton.setEnabled(false);          // Just for display so far
+
+        mSolvedCheckbox = (CheckBox) v.findViewById(R.id.crimed_solved);
+        mSolvedCheckbox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener()
+        {
+            /**
+             * Called when the checked state of a compound button has changed.
+             *
+             * @param buttonView The compound button view whose state has changed.
+             * @param isChecked  The new checked state of buttonView.
+             */
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked)
+            {
+                mCrime.setSolved(isChecked);
+            }
+
+    });
+
+        return v;
+    }
+
+}
